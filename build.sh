8f746254355d2150348e01e39c06703c1af30e90
Milosz Linkiewicz <milosz.linkiewicz@intel.com>
2024-08-10 05:00:48 +0000

    Fisrt changes to the module

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ ENV_PROXY_ARGS=("--progress=plain")
 while IFS='' read -r line; do ENV_PROXY_ARGS+=("--build-arg"); ENV_PROXY_ARGS+=("$line"); done < <(compgen -e | grep -E "_(proxy|PROXY)")
 
 IMAGE_CACHE_REGISTRY="${IMAGE_CACHE_REGISTRY:-docker.io/library}"
-IMAGE_REGISTRY="${IMAGE_REGISTRY:-docker.io/}"
+IMAGE_REGISTRY="${IMAGE_REGISTRY:-docker.io}"
 IMAGE_TAG="${IMAGE_TAG:-latest}"
 NETBOOT_FULL_TAG="${NETBOOT_FULL_TAG:-${IMAGE_REGISTRY}/netboot-xyz:${IMAGE_TAG}}"
 
--- run.sh
+++ run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -eo pipefail
+set -exo pipefail
 SCRIPT_DIR="$(readlink -f "$(dirname -- "${BASH_SOURCE[0]}")")"
 
 RUN_PROXY_ARGS=("-d" "-u" "0:0" "--name=netbootxyz" "--net=host" "--privileged" "--restart" "unless-stopped")
 RUN_PROXY_ARGS+=("-e" "http_proxy=${http_proxy}" "-e" "https_proxy=${https_proxy}" "-e" "no_proxy=${no_proxy}" "-e" "TZ=Europe/Warsaw")
 
-IMAGE_REGISTRY="${IMAGE_REGISTRY:-docker.io/}"
+IMAGE_REGISTRY="${IMAGE_REGISTRY:-docker.io}"
 IMAGE_TAG="${IMAGE_TAG:-latest}"
 NETBOOT_FULL_TAG="${NETBOOT_FULL_TAG:-${IMAGE_REGISTRY}/netboot-xyz:${IMAGE_TAG}}"
 
@@ -14,6 +14,7 @@ docker run "${RUN_PROXY_ARGS[@]}" "$@" \
     -e MENU_VERSION=2.0.80 \
     -e PORT_RANGE=30000:30010 \
     -e NGINX_PORT=80 \
+    -e TFTPD_OPTS='--tftp-single-port' \
     -e WEB_APP_PORT=3000 \
     -v /opt/netboot-xyz/config:/config \
     -v /opt/netboot-xyz/assets:/assets \
